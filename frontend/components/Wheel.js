f073cb5cc0ba6a6b30b64323c069eafb689a453a
Colya Foxworth <cfox1106@gmail.com>
2022-05-27 20:30:24 -0500

    created wheel in state

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,28 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { moveClockwise } from '../state/action-creators'
 import { useDispatch } from 'react-redux'
 
+//action creators
 const aC = {
       moveClockwise,
     }
-  
+
+
 export function Wheel(props) {
   const dispatch = useDispatch()
-
+  const [petals, setActivePetal] = useState(['','','','','',''])
   function handleClick() {
     dispatch({type: 'MOVE_CLOCKWISE'});
-    //if ( e.target.id === props.wheel ) { console.log(e.target) }
+    console.log(petals)
   }
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div id={0} className="cog active" style={{ "--i": 0 }}>B</div>
-        <div id={1} className="cog" style={{ "--i": 1 }}></div>
-        <div id={2} className="cog" style={{ "--i": 2 }}></div>
-        <div id={3} className="cog" style={{ "--i": 3 }}></div>
-        <div id={4} className="cog" style={{ "--i": 4 }}></div>
-        <div id={5} className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+        {petals.map((item, idx) => { return <div className={item === 'B' ? "cog active" : "cog"} style={{ "--i": idx }}>{item === 'B' ? 'B' : ''}</div>})}
       </div>
       <div id="keypad">
         <button id="counterClockwiseBtn" >Counter clockwise</button>
